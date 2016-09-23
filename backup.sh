0655040f742b12152702ef6fc8d168f80e1cbcaf
wenqingqaq <1169125055@qq.com>
2016-09-23 14:54:35 +0800

    添加脚本

---
+++ backup.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+
+send=date '+%Y-%m-%d';
+mysqldump -uroot -phaotoufa2016*we product_fafu > /home/mysql_backup/product_fafu_$send.sql;
+mysqldump -uroot -phaotoufa2016*we fafu_platform > /home/mysql_backup/product_fafu_$send.sql;
