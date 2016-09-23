54f0fa1a0d27d0497df59db6bc48bd371c72084d
wenqingqaq <1169125055@qq.com>
2016-09-23 15:29:50 +0800

    完善脚本请求

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
-!/bin/bash
-send=date '+%Y-%m-%d';
+#!/bin/bash
+echo '开始备份数据库！';
+send=`date +%Y-%m-%d`;
 mysqldump -uroot -phaotoufa2016*we product_fafu > /home/mysql_backup/product_fafu_$send.sql;
-mysqldump -uroot -phaotoufa2016*we fafu_platform > /home/mysql_backup/product_fafu_$send.sql;
+mysqldump -uroot -phaotoufa2016*we fafu_platform > /home/mysql_backup/pre_fafu_$send.sql;
