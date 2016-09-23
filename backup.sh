910225977d7a6f8c71a92be0bb1caa197cd868d1
wenqingqaq <1169125055@qq.com>
2016-09-23 14:57:16 +0800

    取消换行

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 !/bin/bash
-
 send=date '+%Y-%m-%d';
 mysqldump -uroot -phaotoufa2016*we product_fafu > /home/mysql_backup/product_fafu_$send.sql;
 mysqldump -uroot -phaotoufa2016*we fafu_platform > /home/mysql_backup/product_fafu_$send.sql;
